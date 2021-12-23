ae4811dcc24ccb0cb7d370d4fb219708fd0ac975
pat smith <soonerpat@gmail.com>
2021-12-23 15:48:09 -0600

    Starter Template finished

--- history.h
+++ history.h
@@ -14,7 +14,7 @@ using namespace std;
 *  - c-string character array for event description
 *  - population as an int, in 1000's
 */
-struct Year {
+struct Year {  
 	int year; // This is the year for this record
 	char event[201]; // supports up to 200 chars, plus null char
 	int population; // this is a number of world population, in 1000's. 
